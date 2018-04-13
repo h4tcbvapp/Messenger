8f2ac177fe5c1e539aeced6d8335184905689b62
John Dulaney <john.s.dulaney1@gmail.com>
2018-04-13 15:39:55 -0500

    added some attempts at style

--- h4tcbvapp/h4tcbvapp/ClientApp/app/components/account/create.component.ts
+++ h4tcbvapp/h4tcbvapp/ClientApp/app/components/account/create.component.ts
@@ -1,11 +1,13 @@
 import { Component, Inject } from '@angular/core';
 import { Http } from '@angular/http';
 import { UserService } from '../../service/user';
+import { ViewEncapsulation } from '@angular/core/src/metadata/view';
 //import { UserService } from '../../services/user';
 //import { User } from '../../model/user';
 @Component({
     selector: 'createaccount',
-    templateUrl: './create.component.html'
+    templateUrl: './create.component.html',
+   
 })
 export class CreateAccountComponent {
     //constructor(private service: UserService) {}
