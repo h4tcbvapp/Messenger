784b04841c6d6e1aa5cb5bbafed460f6bb10b5d6
John Dulaney <john.s.dulaney1@gmail.com>
2018-04-13 10:24:17 -0500

    mc

--- h4tcbvapp/h4tcbvapp/ClientApp/app/service/user.ts
+++ h4tcbvapp/h4tcbvapp/ClientApp/app/service/user.ts
@@ -19,38 +19,38 @@ export class UserService {
          .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
       }
       
-      public async createUser(userName: string, password: string): Promise<User[]> {
-          const body = JSON.stringify(User);
-          const header = new Headers();
-          header.append(
-                  "Content-Type",
-                  "application/json",
-                  );
-          const response = await this.http.post(this.apiUrl + '/api/user', body , { headers: header });
-                return response.json() as User[];
-            }
+    //   public async createUser(userName: string, password: string): Promise<User[]> {
+    //       const body = JSON.stringify(User);
+    //       const header = new Headers();
+    //       header.append(
+    //               "Content-Type",
+    //               "application/json",
+    //               );
+    //       const response = await this.http.post(this.apiUrl + '/api/user', body , { headers: header });
+    //             return response.json() as User[];
+    //         }
 
 
-            public async loginWithToken(username: string, password: string): Promise<User[]> {
-                  return this.http.post('/api/Token', JSON.stringify({ username: username, password: password }))
-                      .map((response: Response) => {
-                          // login successful if there's a jwt token in the response
-                          const token = response.json() && response.json().token;
-                          if (token) {
-                              // set token property
-                              this.token = token;
-          
-                              // store username and jwt token in local storage to keep user logged in between page refreshes
-                              localStorage.setItem('currentUser', JSON.stringify({ username: username, token: token }));
-          
-                              // return true to indicate successful login
-                              return true;
-                          } else {
-                              // return false to indicate failed login
-                              return false;
-                          }
-                      });
-              }
+    public loginWithToken(username: string, password: string): Observable<boolean> {
+            return this.http.post('/api/Token', JSON.stringify({ username: username, password: password }))
+                .map((response: Response) => {
+                    // login successful if there's a jwt token in the response
+                    let token = response.json() && response.json().token;
+                    if (token) {
+                        // set token property
+                        token = token;
+    
+                        // store username and jwt token in local storage to keep user logged in between page refreshes
+                        localStorage.setItem('currentUser', JSON.stringify({ username: username, token: token }));
+    
+                        // return true to indicate successful login
+                        return true;
+                    } else {
+                        // return false to indicate failed login
+                        return false;
+                    }
+                });
+        }
 
 
 
