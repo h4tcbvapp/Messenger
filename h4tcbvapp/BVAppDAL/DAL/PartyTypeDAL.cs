bf869832dffdf705a685154b62f6cccb08149fb0
Paul Cox <pccox61@gmail.com>
2018-04-13 15:55:49 -0500

    Added PartyTypesDAL class

---
+++ h4tcbvapp/BVAppDAL/DAL/PartyTypeDAL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using BVAppDAL.Models;
+
+namespace BVAppDAL.DAL
+{
+    public class PartyTypeDAL
+    {
+        public string GetPartyTypes()
+        {
+            using (var context = new bvappContext())
+            {
+                var query = (from b in context.PartyType
+                             select b);
+                if (query != null)
+                    return JsonConvert.SerializeObject(query);
+            }
+
+            return string.Empty;
+        }
+    }
+}
