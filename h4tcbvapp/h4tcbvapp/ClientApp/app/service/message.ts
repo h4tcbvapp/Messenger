5b1e69d71650803915294aa515f2d88ad051078d
Hughes, Jonathan <jonathan.hughes@asurion.com>
2018-04-12 21:54:00 -0500

    Refactored message to model / service.

---
+++ h4tcbvapp/h4tcbvapp/ClientApp/app/service/message.ts
@@ -0,0 +1,17 @@
+﻿import { Http } from '@angular/http';
+import { Message } from '../model/message';
+
+export class MessageService {
+
+    private string baseUrl;
+
+    constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
+        this.baseUrl = baseUrl;
+    }
+
+    public async getMessages(student: string): Promise<Message> {
+        const response = await this.http.get(this.baseUrl + 'api/messages').toPromise();
+        return response.json() as Message[];
+    }
+
+}
