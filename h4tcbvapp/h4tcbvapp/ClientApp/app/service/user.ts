3a75c0f42509f6109623699f6d6a00c4b7de4d6f
John Dulaney <john.s.dulaney1@gmail.com>
2018-04-13 10:07:25 -0500

    pre mess

--- h4tcbvapp/h4tcbvapp/ClientApp/app/service/user.ts
+++ h4tcbvapp/h4tcbvapp/ClientApp/app/service/user.ts
@@ -11,39 +11,81 @@ export class UserService {
 
       private apiUrl = 'https://h4tcbvapp.azurewebsites.net'
       constructor(private http: Http){ }
+
       // list users (Admin functionality)
       getUser(): Observable<User[]>  {
          return this.http.get(this.apiUrl + '/api/user')
          .map((res:Response) => res.json())
          .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
       }
-
       
       public async createUser(userName: string, password: string): Promise<User[]> {
           const body = JSON.stringify(User);
           const header = new Headers();
-          header.append("Content-Type","application/json");
+          header.append(
+                  "Content-Type",
+                  "application/json",
+                  );
           const response = await this.http.post(this.apiUrl + '/api/user', body , { headers: header });
                 return response.json() as User[];
             }
 
-      // edit existing user details
-      public async editUser(userName: string, password: string): Promise<User[]>  {
-            const body = JSON.stringify(User);
-            const header = new Headers();
-            header.append("Content-Type","application/json");
-            const response = await this.http.post(this.apiUrl + '/api/user', body , { headers: header }).toPromise();
-                  return response.json() as User[];
-      }
 
-      // delete existing user (deactivate)
-      public async deleteUser(userName: string, password: string): Promise<User[]>  {
-            const options = JSON.stringify(User);
-            const header = new Headers();
-            header.append("Content-Type","application/json");
-            const response = await this.http.delete(this.apiUrl + '/api/user', options , { headers: header }).toPromise();
-                  return response.json() as User[];
-      }
+            public async loginWithToken(username: string, password: string): Promise<User[]> {
+                  return this.http.post('/api/Token', JSON.stringify({ username: username, password: password }))
+                      .map((response: Response) => {
+                          // login successful if there's a jwt token in the response
+                          const token = response.json() && response.json().token;
+                          if (token) {
+                              // set token property
+                              this.token = token;
+          
+                              // store username and jwt token in local storage to keep user logged in between page refreshes
+                              localStorage.setItem('currentUser', JSON.stringify({ username: username, token: token }));
+          
+                              // return true to indicate successful login
+                              return true;
+                          } else {
+                              // return false to indicate failed login
+                              return false;
+                          }
+                      });
+              }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      // // edit existing user details
+      // public async editUser(userName: string, password: string): Promise<User[]>  {
+      //       const body = JSON.stringify(User);
+      //       const header = new Headers();
+      //       header.append("Content-Type","application/json");
+      //       const response = await this.http.post(this.apiUrl + '/api/user', body , { headers: header }).toPromise();
+      //             return response.json() as User[];
+      // }
+
+      // // delete existing user (deactivate)
+      // public async deleteUser(userName: string, password: string): Promise<User[]>  {
+      //       const options = JSON.stringify(User);
+      //       const header = new Headers();
+      //       header.append("Content-Type","application/json");
+      //       const response = await this.http.delete(this.apiUrl + '/api/user', options , { headers: header }).toPromise();
+      //             return response.json() as User[];
+      // }
 }
 
 
